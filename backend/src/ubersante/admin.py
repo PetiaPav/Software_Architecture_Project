81a7418fdd98e99cea22c620622b84ad8d7f39e2
Vineet <koppalkarvineet@gmail.com>
2019-02-05 01:34:41 -0500

    [#7] Implmented patient model and setup patient dashboard

--- backend/src/ubersante/admin.py
+++ backend/src/ubersante/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from ubersante.models import Appointment
+from ubersante.models import Appointment, Patient
+
 
 # This is used to display Appointments as a table instead of a list in the admin dashboard
 class AppointmentsAdmin(admin.ModelAdmin):
@@ -15,4 +16,23 @@ class AppointmentsAdmin(admin.ModelAdmin):
     search_fields = ('time', 'content')
     list_per_page = 25
 
+
+# This is used to display Patients in a table format in the admin dashboard
+class PatientAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'health_card_number',
+        'first_name',
+        'last_name',
+        'phone_number',
+        'email'
+    )
+
+    list_display_links = ('id', 'health_card_number')
+    search_fields = ('health_card_number', 'content')
+    list_per_page = 25
+
+
 admin.site.register(Appointment, AppointmentsAdmin)
+admin.site.register(Patient, PatientAdmin)
+
