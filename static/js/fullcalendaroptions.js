e8a8fa8c02c6d14e5838b3251fe5d58842c1963d
Vineet <koppalkarvineet@gmail.com>
2019-02-16 23:57:56 -0500

    [#12] Added draggable walk-in events

--- static/js/fullcalendaroptions.js
+++ static/js/fullcalendaroptions.js
@@ -1,5 +1,21 @@
 // this runs after the page has been initialized
 $(document).ready(function() {
+
+    $('#external-events .fc-event').each(function() {
+        // store data so the calendar knows to render an event upon drop
+        $(this).data('event', {
+            title: $.trim($(this).text()), // use the element's text as the event title
+            stick: true, // maintain when user navigates (see docs on the renderEvent method)
+            duration: '03:00:00'
+        });
+        // make the event draggable using jQuery UI
+        $(this).draggable({
+            zIndex: 999,
+            revert: true,      // will cause the event to go back to its
+            revertDuration: 0  //  original position after the drag
+        });
+    });
+
     $('#calendar').fullCalendar({
         // Define fullcalendar license key
         schedulerLicenseKey: 'CC-Attribution-NonCommercial-NoDerivatives',
@@ -23,9 +39,10 @@ $(document).ready(function() {
         // Height of calendar
         contentHeight: 1000,
 
+
         // Grey out non-business hours
         businessHours: {
-            dow: [0, 1, 2, 3, 4, 5, 6],   // Days of the week - Mon to Fri
+            dow: [ 1, 2, 3, 4, 5],   // Days of the week - Mon to Fri
             start: "08:00:00",
             end: "20:00:00",
         },
@@ -33,12 +50,13 @@ $(document).ready(function() {
         // Frequency for displaying time slots, in minutes (20 minute partitions)
         slotDuration: "00:20:00",
 
+        allDay: true,
+
         // Retrieve events using /data route
         events: { url: 'data' },
 
-         // onClick of an event
+        // onClick of an event
 		eventClick: function(eventObj) {
-		    alert("Clicked " + eventObj.title)
             $.ajax({
                 url: 'data',
                 type: 'POST',
@@ -51,7 +69,16 @@ $(document).ready(function() {
             });
 		},
 
+		editable: true,
+        droppable: true, // this allows things to be dropped onto the calendar
+        drop: function() {
+            $(this).remove();
 
-
+//            // is the "remove after drop" checkbox checked?
+//            if ($('#drop-remove').is(':checked')) {
+//                // if so, remove the element from the "Draggable Events" list
+//                $(this).remove();
+//            }
+        },
     });
-})
+});
