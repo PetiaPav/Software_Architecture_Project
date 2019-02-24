a968b065b27211d6fc2fcff6f62ca1d4b1249946
PetiaPav <petia2397@gmail.com>
2019-02-24 12:42:00 -0500

    [#2] Login Forms added for the different types of users
    Co-authored-by: Carlo <lucastle6969@users.noreply.github.com>

--- model/Forms.py
+++ model/Forms.py
@@ -1,5 +1,7 @@
 from wtforms import Form, StringField, DateField, SelectField, TextAreaField, PasswordField, validators
 
+def valid_access_id (form, field):
+
 
 class RegisterForm(Form):
     email = StringField('Email')
@@ -16,10 +18,25 @@ class RegisterForm(Form):
     physical_address = StringField('Address')
 
 
+class LoginForm(Form):
+    password = PasswordField('Password', [Length(min=6)])
+
+
+class LoginDoctorForm(LoginForm):
+    physician_permit_number = StringField('Physician Permit Number', Length=7)
+
+
+class LoginNurseForm(LoginForm):
+    access_id = StringField('Access ID', Length=8, valid_access_id)
+
+
+class LoginPatientForm(LoginForm):
+    email = StringField('Email', InputRequired())
+
+
 class AppointmentForm(Form):
     doctor_id = SelectField('Doctor Name', choices=[('1', 'Ivanov'), ('2', 'Petrov')])
     room = SelectField('Room Number', choices=[('1', '1'), ('2', '2')])
     # TODO will need to properly validate and convert the times
     start_time = StringField('Start Time')
     end_time = StringField('Start Time')
-
