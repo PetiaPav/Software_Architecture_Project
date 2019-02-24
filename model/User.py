2d9b40396fe5107f3dc52a198073e001824146b8
lucastle6969 <lucastle69@hotmail.com>
2019-02-24 10:31:07 -0500

    [#2] Skeleton for User.py initial hierarchy and attributes
    Co-authored-by: Petia <PetiaPav@users.noreply.github.com>
    Co-authored-by: Suzanne <Mstc08@users.noreply.github.com>
    Co-authored-by: Kevin <kjanei@users.noreply.github.com>
    Co-authored-by: Vineet <vineetkoppalkar@users.noreply.github.com>
    Co-authored-by: Michael <Michael-Gagnon@users.noreply.github.com>
    Co-authored-by: Dmitry <dfeokti@users.noreply.github.com>

---
+++ model/User.py
@@ -0,0 +1,46 @@
+class User:
+    def __init__(self, id, first_name, last_name, password):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password
+
+    def is_authenticated(self):
+        None
+
+    def is_active(self):
+        None
+
+    def is_anonymous(self):
+        None
+
+    def get_id(self):
+        None
+
+
+class Patient(User):
+    def __init__(self, id, first_name, last_name, password, health_card, birthday, gender, phone, physical_address, email, cart):
+        User.__init__(self, id, first_name, last_name, password)
+        self.health_card = health_card
+        self.birthday = birthday
+        self.gender = gender
+        self.phone = phone
+        self.physical_address = physical_address
+        self.email = email
+        self.cart = cart
+
+
+class Nurse(User):
+    def __init__(self, id, first_name, last_name, password, access_id):
+        User.__init__(self, id, first_name, last_name, password)
+        self.access_id = access_id
+
+
+class Doctor(User):
+    def __init__(self, id, first_name, last_name, password, physician_permit_number, specialty, city, availability, appointments):
+        User.__init__(self, id, first_name, last_name, password)
+        self.physician_permit_number = physician_permit_number
+        self.specialty = specialty
+        self.city = city
+        self.availability = availability
+        self.appointments = appointments
