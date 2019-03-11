7e1f361baa1b5b0d79a7d7112bca667ed5542711
lucastle6969 <lucastle69@hotmail.com>
2019-03-10 23:56:03 -0400

    methods to get and set doctor availabilities untested

--- model/Tdg.py
+++ model/Tdg.py
@@ -112,6 +112,17 @@ class Tdg:
         cur.close()
         return doctors
 
+    def update_doctor_availability(self, doctor_id, list_of_availabilities):
+        connection = self.mysql.connect()
+        cur = connection.cursor()
+        # delete all existing availability for this doctor
+        cur.execute("DELETE FROM DOCTOR_AVAILABILITIES WHERE doctor_id=%s", [doctor_id])
+        # populate new availability
+        for avail in list_of_availabilities:
+            cur.execute("INSERT INTO DOCTOR_AVAILABILITIES (id, doctor_id, day_index, slot_index, walk_in) VALUES (NULL, %s, %s, %s, %s)", (avail['doctor_id'], avail['day'], avail['slot_index'], avail['walk_in']))
+        connection.commit()
+        cur.close()
+
     def get_doctor_availabilities(self, doctor_id):
         connection = self.mysql.connect()
         cur = connection.cursor()
