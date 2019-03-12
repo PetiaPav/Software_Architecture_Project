31a0b4779a4f10f6b09c528bdb312a3aacd4d137
dfeokti <d_feokti@live.concordia.ca>
2019-03-11 23:27:28 -0400

    reg/login of multiple users

--- tests/unit/test_registration.py
+++ tests/unit/test_registration.py
@@ -19,3 +19,46 @@ def test_login_patient(client):
     rv2 = client.get('/logout', follow_redirects=True)
     assert rv2.status_code == 200
     assert b'You are now logged out!' in rv2.data
+    assert 'logged in' not in session
+
+
+def test_reg_nurse(client):
+    data = dict([ ('password', '1q2w3e'), ('confirm', '1q2w3e'), ('first_name', 'Ivana'),
+                 ('last_name', 'Ivanova'), ('access_id', 'CBA12345')])
+    rv = client.post('/register/nurse', data=data, follow_redirects=True)
+    assert rv.status_code == 200
+    assert b'You are now registered and can log in!' in rv.data
+
+
+def test_login_nurse(client):
+    data = dict([('access_id', 'CBA12345'), ('password', '1q2w3e')])
+    rv = client.post('/login/nurse', data=data, follow_redirects=True)
+    assert rv.status_code == 200
+    assert b'Hello Ivana, Welcome' in rv.data
+    assert session['logged_in'] is True
+
+    rv2 = client.get('/logout', follow_redirects=True)
+    assert rv2.status_code == 200
+    assert b'You are now logged out!' in rv2.data
+    assert 'logged in' not in session
+
+
+def test_reg_doctor(client):
+    data = dict([ ('password', 'q1w2e3'), ('confirm', 'q1w2e3'), ('first_name', 'Peter'),
+                  ('last_name', 'Petrov'), ('permit_number', '1245789')])
+    rv = client.post('/register/doctor', data=data, follow_redirects=True)
+    assert rv.status_code == 200
+    assert b'You are now registered and can log in!' in rv.data
+
+
+def test_login_doctor(client):
+    data = dict([('permit_number', '1245789'), ('password', 'q1w2e3')])
+    rv = client.post('/login/doctor', data=data, follow_redirects=True)
+    assert rv.status_code == 200
+    assert b'Hello Peter, Welcome' in rv.data
+    assert session['logged_in'] is True
+
+    rv2 = client.get('/logout', follow_redirects=True)
+    assert rv2.status_code == 200
+    assert b'You are now logged out!' in rv2.data
+    assert 'logged in' not in session
