82d2294a8009328a00044b54dca8b97260f98a2e
dfeokti <d_feokti@live.concordia.ca>
2019-04-02 00:46:54 -0400

    Appointments queries

--- model/Tdg.py
+++ model/Tdg.py
@@ -258,11 +258,13 @@ class Tdg:
         cur.execute("UPDATE PATIENTS LEFT JOIN USERS ON PATIENTS.user_fk = USERS.id SET first_name = %s, last_name = %s, health_card = %s, birthday = %s, gender = %s, phone_number = %s, physical_address = %s, email = %s WHERE PATIENTS.id = %s", (first_name, last_name, health_card, birthday, gender, phone_number, physical_address, email, id))
         cur.close()
 
-    def insert_appointment(self, id, clinic_id, room_id, doctor_id, patient_id, date_time, walk_in):
+    def insert_appointment(self, clinic_id, room_id, doctor_id, patient_id, date_time, walk_in):
         connection = self.mysql.connect()
         cur = connection.cursor()
         cur.execute( "INSERT INTO APPOINTMENTS (id, clinic_id, room_id, doctor_id, patient_id, date_time, walk_in) VALUES (NULL, %s, %s, %s, %s, %s, %s)",(clinic_id, room_id, doctor_id, patient_id, date_time, walk_in))
+        last_inserted_id = cur.lastrowid
         cur.close()
+        return last_inserted_id
 
     def get_all_appointments(self):
         connection = self.mysql.connect()
