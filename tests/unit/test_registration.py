0bf7e92ee0a4e445f993bd04cdaeb40f07070fc4
dfeokti <d_feokti@live.concordia.ca>
2019-04-11 09:05:07 -0400

    Data fix to stop errors.

--- tests/unit/test_registration.py
+++ tests/unit/test_registration.py
@@ -5,7 +5,7 @@ from flask import session
 def test_reg_patient(client):
     data = dict([('email', 'ivan@mail.ru'), ('password', 'q1w2e3'), ('confirm', 'q1w2e3'), ('first_name', 'Ivan'),
                  ('last_name', 'Ivanov'), ('health_card', 'IVAN 1234 0987'), ('phone_number', '(123) 456-7890'),
-                 ('birthday', '01/01/80'), ('gender', 'M'), ('physical_address', '123 Main Street')])
+                 ('birthday', '01/01/1980'), ('gender', 'M'), ('physical_address', '123 Main Street')])
     rv = client.post('/register/patient', data=data, follow_redirects=True)
     assert rv.status_code == 200
     assert b'You are now registered and can log in!' in rv.data
@@ -13,7 +13,7 @@ def test_reg_patient(client):
 def test_dup_patient(client):
     data = dict([('email', 'ivan@mail.ru'), ('password', 'q1w2e3'), ('confirm', 'q1w2e3'), ('first_name', 'Ivan'),
                  ('last_name', 'Ivanov'), ('health_card', 'IVAN 1234 0987'), ('phone_number', '(123) 456-7890'),
-                 ('birthday', '01/01/80'), ('gender', 'M'), ('physical_address', '123 Main Street')])
+                 ('birthday', '01/01/1980'), ('gender', 'M'), ('physical_address', '123 Main Street')])
     rv = client.post('/register/patient', data=data, follow_redirects=True)
     assert rv.status_code == 200
     assert b'This e-mail address has already been registered.' in rv.data
@@ -26,6 +26,8 @@ def test_login_patient(client):
     assert b'Hello Ivan, Welcome' in rv.data
     assert session['logged_in'] is True
 
+
+def test_logout_patient(client):
     rv2 = client.get('/logout', follow_redirects=True)
     assert rv2.status_code == 200
     assert b'You are now logged out!' in rv2.data
